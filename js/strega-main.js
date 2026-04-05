ae53878c161b2ba76f0c4e21aa18091b218d1d5e
wightryder <github@solentsoftware.co.uk>
2026-04-05 13:39:08 +0100

    strega Update

--- js/strega-main.js
+++ js/strega-main.js
@@ -1,6 +1,26 @@
 //strega using Oakwood Framework v4.1.0
 class SiteFooter extends HTMLElement {
     connectedCallback() {
+        this.render();
+    }
+
+    render() {
+        this.innerHTML = `
+            <footer class="footer-site">
+                <div class="footer-item">
+                    p><strong><p>📞 <a href="tel:+44 7801434050">+44 (0)7801 434050</a></strong></p>
+                <p>✉️ <a href="mailto:reachme@stregahealth.com">reachme@stregahealth.com</a></p>
+                <p>&copy; 2026 Strega Health. All rights reserved.</p>
+                </div>
+            </footer>
+        `;
+    }
+}
+
+
+
+class SiteFooter extends HTMLElement {
+    connectedCallback() {
         this.innerHTML = `
             <div class="item-footer">
                 <p><strong><p>📞 <a href="tel:+44 7801434050">+44 (0)7801 434050</a></strong></p>
